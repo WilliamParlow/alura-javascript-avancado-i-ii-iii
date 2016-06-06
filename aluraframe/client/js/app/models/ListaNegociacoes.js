13de2889b717c2a89f81b006643c064272f0f488
geisonsn <geison.sn@gmail.com>
2016-06-06 11:52:36 -0400

    Uso de Arrow Function para corrigir o contexto do this na função callback definida no construtor da classe ListaNegociacoes

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,13 @@
 class ListaNegociacoes {
 
-    constructor(contexto, armadilha) {
+    constructor(armadilha) {
         this._negociacoes = [];
         this._armadilha = armadilha;
-        this._contexto = contexto;
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
-        //this._armadilha(this);
+        this._armadilha(this);
     }
 
     get negociacoes() {
@@ -18,7 +16,6 @@ class ListaNegociacoes {
 
     esvazia() {
         this._negociacoes = [];
-        Reflect.apply(this._armadilha, this._contexto, [this]);
-        //this._armadilha(this);
+        this._armadilha(this);
     }
 }
